b1ab35ca56b50bd88e568392f9c6b3f8a89aea4a
vbsantos <vbsanttos@gmail.com>
2022-02-21 21:27:40 -0300

    config express

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express, { Application } from 'express';
 import { routes } from './routes';
 
 const app: Application = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(routes);
 
 export { app };
