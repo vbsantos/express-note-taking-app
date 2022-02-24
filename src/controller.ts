acb45078d835fcea5591027028938e0db28b112b
vbsantos <vbsanttos@gmail.com>
2022-02-24 00:44:05 -0300

    add controller and database class

---
+++ src/controller.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from 'express';
+
+import { INote, IDatabase } from './database';
+
+export class Controller {
+  private database: IDatabase;
+
+  constructor(injectedDatabase: IDatabase) {
+    this.database = injectedDatabase;
+  }
+
+  // store
+  storeNote = (req: Request, res: Response) => {
+    const { title, content } = req.body;
+    this.database.storeNote(title, content);
+    res.redirect('/notes');
+  };
+
+  // update
+  updateNote = (req: Request, res: Response) => {
+    const { noteId, title, content } = req.body;
+    this.database.updateNote(noteId, title, content);
+    res.redirect(`/notes/${noteId}`);
+  };
+
+  // delete
+  deleteNote = (req: Request, res: Response) => {
+    const { noteId } = req.body;
+    this.database.deleteNote(noteId);
+    res.redirect('/notes');
+  };
+
+  // redirect to the main page
+  goToMainPage = (_req: Request, res: Response) => {
+    res.redirect('/notes');
+  };
+
+  // index view - renders notes page
+  notesView = (req: Request, res: Response) => {
+    throw new Error('asd');
+
+    const previewStringSize = 175;
+    const { search } = req.query;
+    const notes = !search
+      ? this.database.getNotes()
+      : this.database.getNotesByText(search.toString());
+    res.render('notes.ejs', { notes, previewStringSize });
+  };
+
+  // show view - renders note page
+  noteByIdView = (req: Request, res: Response) => {
+    const { noteId } = req.params;
+    const note: INote = this.database.getNoteById(noteId);
+    if (!note) throw new Error('PAGE_NOT_FOUND');
+    res.render('note.ejs', { note });
+  };
+
+  // store view - renders a page with a form to add a note
+  storeNoteView = (_req: Request, res: Response) => {
+    res.render('storeNote.ejs');
+  };
+
+  // edit view - renders a page with a form for editing a note
+  getNoteByIdEditView = (req: Request, res: Response) => {
+    const { noteId } = req.params;
+    const note: INote = this.database.getNoteById(noteId);
+    if (!note) throw new Error('PAGE_NOT_FOUND');
+    res.render('editNote.ejs', { note });
+  };
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,94 +1,27 @@
-import {
-  Router,
-  Request,
-  Response,
-  ErrorRequestHandler,
-  NextFunction,
-} from 'express';
+import { Router } from 'express';
 
-import {
-  INote,
-  getNoteById,
-  getNotes,
-  addNote,
-  deleteNote,
-  getNotesByText,
-  updateNote,
-} from './database';
+import { Database } from './database';
+import { Controller } from './controller';
+
+const database = new Database();
+const controller = new Controller(database);
 
 const routes: Router = Router();
 
-routes.get('/', (_req: Request, res: Response) => {
-  res.redirect('/notes');
-});
+routes.get('/', controller.goToMainPage);
 
-routes.get('/notes', (req: Request, res: Response) => {
-  const previewSize: number = 175;
-  const { search } = req.query;
-  const notes = !search ? getNotes() : getNotesByText(search.toString());
-  res.render('notes.ejs', { notes, previewSize });
-});
+routes.get('/notes', controller.notesView);
 
-routes.get('/notes/add', (_req: Request, res: Response) => {
-  res.render('addNote.ejs');
-});
+routes.get('/notes/add', controller.storeNoteView);
 
-routes.get('/notes/:noteId', (req: Request, res: Response) => {
-  const { noteId } = req.params;
-  const note: INote = getNoteById(noteId);
-  if (!note) {
-    const error = {
-      code: 404,
-      body: 'Error 404: Sorry, note not found 📖',
-    };
-    res.status(error.code).render('error.ejs', { error });
-    return;
-  }
-  res.render('note.ejs', { note });
-});
+routes.get('/notes/:noteId', controller.noteByIdView);
 
-routes.get('/notes/:noteId/edit', (req: Request, res: Response) => {
-  const { noteId } = req.params;
-  const note: INote = getNoteById(noteId);
-  if (!note) {
-    const error = {
-      code: 404,
-      body: 'Error 404: Sorry, note not found 📖',
-    };
-    res.status(error.code).render('error.ejs', { error });
-    return;
-  }
-  res.render('editNote.ejs', { note });
-});
+routes.get('/notes/:noteId/edit', controller.getNoteByIdEditView);
 
-routes.post('/notes/store', (req: Request, res: Response) => {
-  const { title, content } = req.body;
-  addNote({ title, content });
-  res.redirect('/notes');
-});
+routes.post('/notes/store', controller.storeNote);
 
-routes.post('/notes/delete', (req: Request, res: Response) => {
-  const { noteId } = req.body;
-  deleteNote(noteId);
-  res.redirect('/notes');
-});
+routes.post('/notes/update', controller.updateNote);
 
-routes.post('/notes/update', (req: Request, res: Response) => {
-  const { noteId, title, content } = req.body;
-  updateNote(noteId, title, content);
-  res.redirect(`/notes/${noteId}`);
-});
-
-routes.use((_req: Request, res: Response, next: NextFunction) => {
-  next();
-  const error = { code: 404, body: 'Error 404: Sorry, page not found 📖' };
-  res.status(error.code).render('error.ejs', { error });
-});
-
-routes.use((err: ErrorRequestHandler, _req: Request, res: Response, next: NextFunction) => {
-  next();
-  const error = { code: 500, body: 'Error 500: Sorry, try again later 📖', details: JSON.stringify(err.toString(), undefined, 2) };
-  res.status(error.code).render('error.ejs', { error });
-});
+routes.post('/notes/delete', controller.deleteNote);
 
 export { routes };
