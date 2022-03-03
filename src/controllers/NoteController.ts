e4ad681b99955a1a685ba2dea86b24ddb452412c
vbsantos <vbsanttos@gmail.com>
2022-03-03 01:37:10 -0300

    add custom error handling

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import { BadRequestError, NotFoundError } from '../errors';
 
 import { IDatabase, INote } from '../types';
 
@@ -13,9 +14,7 @@ export class NoteController {
   }
 
   // redirect to the main page
-  goToMainPage = (_req: Request, res: Response) => {
-    res.redirect('/notes');
-  };
+  goToMainPage = (_req: Request, res: Response) => res.redirect('/notes');
 
   // index view - renders notes page
   notesView = async (req: Request, res: Response) => {
@@ -24,7 +23,7 @@ export class NoteController {
     const notes: INote[] = hasFilter
       ? await this.database.getNotesByText(search.toString())
       : await this.database.getNotes();
-    res.render('notes.ejs', { notes, previewStringSize: this.previewStringSize, hasFilter });
+    return res.render('notes.ejs', { notes, previewStringSize: this.previewStringSize, hasFilter });
   };
 
   // show view - renders note page
@@ -32,50 +31,58 @@ export class NoteController {
     const { noteId } = req.params;
     const note: INote = await this.database.getNoteById(noteId);
     if (!note) {
-      next();
-      return;
+      return next(new NotFoundError('Note not found'));
     }
-    res.render('note.ejs', { note });
+    return res.render('note.ejs', { note });
   };
 
   // store view - renders a page with a form to add a note
-  storeNoteView = (_req: Request, res: Response) => {
-    res.render('storeNote.ejs');
-  };
+  storeNoteView = (_req: Request, res: Response) => res.render('storeNote.ejs');
 
   // edit view - renders a page with a form for editing a note
   updateNoteView = async (req: Request, res: Response, next: NextFunction) => {
     const { noteId } = req.params;
     const note: INote = await this.database.getNoteById(noteId);
     if (!note) {
-      next();
-      return;
+      return next(new NotFoundError('Note not found'));
     }
-    res.render('editNote.ejs', { note });
+    return res.render('editNote.ejs', { note });
   };
 
   // store
   storeNote = async (req: Request, res: Response, next: NextFunction) => {
     const { title, content } = req.body;
-    if (!title || !content) next();
+    if (!title || !content) {
+      // REVIEW is it working?
+      return next(new BadRequestError('Required field is missing', {
+        note: { title, content },
+        at: 'storeNote',
+      }));
+    }
     const noteId = uuidv4();
     await this.database.storeNote(noteId, title, content);
-    res.redirect('/notes');
+    return res.redirect('/notes');
   };
 
   // update
   updateNote = async (req: Request, res: Response, next: NextFunction) => {
     const { title, content } = req.body;
-    if (!title || !content) next();
+    if (!title || !content) {
+      // REVIEW is it working?
+      return next(new BadRequestError('Required field is missing', {
+        note: { title, content },
+        at: 'editNote',
+      }));
+    }
     const { noteId } = req.params;
     await this.database.updateNote(noteId, title, content);
-    res.redirect(`/notes/${noteId}`);
+    return res.redirect(`/notes/${noteId}`);
   };
 
   // delete
   deleteNote = async (req: Request, res: Response) => {
     const { noteId } = req.params;
     await this.database.deleteNote(noteId);
-    res.redirect('/notes');
+    return res.redirect('/notes');
   };
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,24 +4,38 @@ import {
   Request,
   Response,
 } from 'express';
+import JWT from 'jsonwebtoken';
+
+import {
+  BadRequestError, DatabaseError, NotFoundError, UnauthorizedError,
+} from './errors';
 
 class Middleware {
-  private sendPageNotFound = (res) => {
-    res.status(404).render('error.ejs', {
+  private sendErrorPage = (response: Response, error) => {
+    response.status(error.statusCode).render(error.view, {
       error: {
-        code: 404,
-        body: 'Error 404: Sorry, page not found 📝',
+        code: error.statusCode,
+        title: `📝 ${error.message}`,
+        description: `Error ${error.statusCode}: ${error.name}`,
+        stack: error.stack,
       },
     });
   };
 
-  pageNotFoundView = (_req: Request, res: Response) => {
-    this.sendPageNotFound(res);
-  };
-
-  serverErrorLogger = (err, _req, _res, next) => {
+  serverErrorLogger = (
+    err: ErrorRequestHandler,
+    _req: Request,
+    _res: Response,
+    next: NextFunction,
+  ) => {
     console.error('[\x1b[31mSERVER ERROR\x1b[0m]', err); // REVIEW LOGGER
     next(err);
+  };
+
+  pageNotFoundView = (_req: Request, res: Response, next: NextFunction) => {
+    next();
+
+    this.sendErrorPage(res, new NotFoundError('This page does not exist'));
   };
 
   serverErrorHandler = (
@@ -32,17 +46,53 @@ class Middleware {
   ) => {
     next();
 
-    if (err.toString() === 'Error: PAGE_NOT_FOUND') {
-      this.sendPageNotFound(res);
+    if (err instanceof UnauthorizedError) {
+      res.redirect('/login');
+    }
+
+    if (err instanceof BadRequestError) {
+      if (err.at === 'login') {
+        res.render('login.ejs', { user: err.user, error: err.message });
+      }
+      if (err.at === 'register') {
+        res.render('register.ejs', { user: err.user, error: err.message });
+      }
+      if (err.at === 'storeNote') {
+        res.render('storeNote.ejs', { note: err.note, error: err.message });
+      }
+      if (err.at === 'editNote') {
+        res.render('editNote.ejs', { note: err.note, error: err.message });
+      }
+    }
+
+    if (err instanceof NotFoundError || err instanceof DatabaseError) {
+      this.sendErrorPage(res, err);
     }
 
     res.status(500).render('error.ejs', {
       error: {
         code: 500,
-        body: 'Error 500: Sorry, try again later 📝',
-        details: err.toString(),
+        title: 'Error 500: Sorry, try again later 📝',
+        description: err.toString(),
       },
     });
+  };
+
+  // FIXME descobrir como adicionar User no tipo Request
+  authenticateToken = async (req: Request, res: Response, next: NextFunction) => {
+    const { authToken } = req.cookies;
+    if (authToken) {
+      try {
+        const { user } = await JWT.verify(authToken, process.env.ACCESS_TOKEN_SECRET);
+        req.user = { _id: user._id, name: user.name, email: user.email };
+        return next();
+      } catch (error) {
+        console.log('[\x1b[31mAUTH ERROR\x1b[0m]', error); // REVIEW LOGGER
+      }
+    }
+
+    return next(new UnauthorizedError('You do not have permission'));
+    // return res.redirect('/login');
   };
 }
 
