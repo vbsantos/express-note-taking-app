cbc6f6582ff3209c21902f6e7fb907f9d04239bc
vbsantos <vbsanttos@gmail.com>
2022-03-15 22:25:34 -0300

    refactor validation schemas

--- src/validation/noteSchema.ts
+++ src/validation/noteSchema.ts
@@ -1,8 +1,10 @@
 import Joi from 'joi';
 
-export const noteSchema = Joi.object({
+const noteValidation = {
   title: Joi.string().min(3).max(100).label('Title')
     .required(),
   content: Joi.string().min(3).label('Content')
     .required(),
-});
+};
+
+export const noteSchema = Joi.object(noteValidation);
--- src/validation/userSchema.ts
+++ src/validation/userSchema.ts
@@ -1,19 +1,20 @@
 import Joi from 'joi';
 
-export const registrationSchema = Joi.object({
-  name: Joi.string().min(3).max(30).label('Name')
-    .required(),
+const loginValidation = {
   email: Joi.string().email().label('E-mail')
     .required(),
   password: Joi.string().min(8).max(30).label('Password')
+    .required(),
+};
+
+const registrationValidation = {
+  ...loginValidation,
+  name: Joi.string().min(3).max(30).label('Name')
     .required(),
   password2: Joi.string().valid(Joi.ref('password')).messages({ 'any.only': 'passwords must match' })
     .required(),
-});
+};
 
-export const loginSchema = Joi.object({
-  email: Joi.string().email().label('E-mail')
-    .required(),
-  password: Joi.string().min(8).max(30).label('Password')
-    .required(),
-});
+export const loginSchema = Joi.object(loginValidation);
+
+export const registrationSchema = Joi.object(registrationValidation);
